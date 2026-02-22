6bddfababcd2dec7f5407f0e5f8a3108ced933d4
abdur-code <abdurrahmanim2422@gmail.com>
2026-02-23 02:17:30 +0530

    Ex5

--- SOLID/Ex5/src/ExportResult.java
+++ SOLID/Ex5/src/ExportResult.java
@@ -1,9 +1,24 @@
 public class ExportResult {
     public final String contentType;
     public final byte[] bytes;
+    public final boolean success;
+    public final String errorMessage;
 
     public ExportResult(String contentType, byte[] bytes) {
         this.contentType = contentType;
         this.bytes = bytes;
+        this.success = true;
+        this.errorMessage = null;
+    }
+
+    private ExportResult(String contentType, byte[] bytes, boolean success, String errorMessage) {
+        this.contentType = contentType;
+        this.bytes = bytes;
+        this.success = success;
+        this.errorMessage = errorMessage;
+    }
+
+    public static ExportResult failure(String message) {
+        return new ExportResult(null, new byte[0], false, message);
     }
 }
